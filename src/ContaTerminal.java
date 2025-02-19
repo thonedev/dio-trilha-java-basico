bdeb165e94a682a7db978e59e5b03074662a422a
Thone Cardoso de Araujo <thonecardoso@gmail.com>
2025-02-19 11:00:29 -0300

    recebendo argumentos pelo terminal: java ContaTerminal 1021 067-8 'MARIO ANDRADE' 237.48

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,13 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero = Integer.parseInt(args[0]);
+        String agencia = args[1];
+        String nomeCliente = args[2];
+        double saldo = Double.parseDouble(args[3]);
+
+        System.err.println("Número da conta: " + numero);
+        System.err.println("Número da agência: " + agencia);
+        System.err.println("Nome do cliente: " + nomeCliente);
+        System.err.println("Saldo da conta: " + saldo);
     }
 }
