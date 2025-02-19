aad35924e57d4e11c79089681f327d3ae6e48a7d
Thone Cardoso de Araujo <thonecardoso@gmail.com>
2025-02-19 12:38:57 -0300

    formatando saída de dados

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -13,9 +13,13 @@ public class ContaTerminal {
         System.out.println("Digite o saldo da conta: ");
         double saldo = Double.parseDouble(sc.nextLine());
 
+        sc.close();
+
         System.err.println("Número da conta: " + numero);
         System.err.println("Número da agência: " + agencia);
         System.err.println("Nome do cliente: " + nomeCliente);
         System.err.println("Saldo da conta: " + saldo);
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque", nomeCliente, agencia, numero, saldo);
     }
 }
